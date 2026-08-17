34d1ea3c61775304181d84be941fad7f4939c393
QBi <95951192+mybeXX@users.noreply.github.com>
2026-08-18 01:55:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+#
+# ImmortalWrt Custom DIY Part2
+#
+
+
 echo "
+
 # Argon主题
 CONFIG_PACKAGE_luci-theme-argon=y
+
 
 # TurboACC
 CONFIG_PACKAGE_luci-app-turboacc=y
 
-# 网络工具
-CONFIG_PACKAGE_ethtool=y
 
 # 网卡驱动
 CONFIG_PACKAGE_kmod-e1000=y
@@ -22,52 +27,108 @@ CONFIG_PACKAGE_kmod-r8169=y
 CONFIG_PACKAGE_kmod-r8125=y
 CONFIG_PACKAGE_kmod-r8126=y
 
+
+# 网络工具
+CONFIG_PACKAGE_ethtool=y
+
+
 " >> .config
 
 
-# 设置默认IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# =========================
+# 默认 LAN IP
+# =========================
+
+sed -i 's/192.168.1.1/10.0.0.1/g' \
+package/base-files/files/bin/config_generate
 
 
-# 保留ImmortalWrt名称
-sed -i 's/OpenWrt/ImmortalWrt/g' package/base-files/files/bin/config_generate
+
+# =========================
+# 保留 ImmortalWrt 名称
+# =========================
+
+sed -i 's/OpenWrt/ImmortalWrt/g' \
+package/base-files/files/bin/config_generate
 
 
-# banner
-cp -f $GITHUB_WORKSPACE/banner package/base-files/files/etc/banner
+
+# =========================
+# Banner
+# =========================
+
+if [ -f "$GITHUB_WORKSPACE/banner" ]; then
+
+cp -f \
+$GITHUB_WORKSPACE/banner \
+package/base-files/files/etc/banner
+
+fi
 
 
-# Argon背景兼容
 
-mkdir -p feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background
+# =========================
+# Argon 自定义壁纸
+# =========================
+
+if [ -f "$GITHUB_WORKSPACE/argon/background/background.jpg" ]; then
+
+
+mkdir -p \
+feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background
+
 
 cp -f \
 $GITHUB_WORKSPACE/argon/background/background.jpg \
 feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background/background.jpg
 
 
+
+# 兼容部分 Argon 版本
+
 cp -f \
 $GITHUB_WORKSPACE/argon/background/background.jpg \
 feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background.jpg
 
 
+fi
+
+
+
+# =========================
 # 默认设置
+# =========================
+
 if [ -f "$GITHUB_WORKSPACE/99-default-settings" ]; then
 
-    mkdir -p package/base-files/files/etc/uci-defaults
 
-    cp -f \
-    $GITHUB_WORKSPACE/99-default-settings \
-    package/base-files/files/etc/uci-defaults/99-default-settings
+mkdir -p \
+package/base-files/files/etc/uci-defaults
+
+
+cp -f \
+$GITHUB_WORKSPACE/99-default-settings \
+package/base-files/files/etc/uci-defaults/99-default-settings
+
 
 fi
 
 
-# 清理重复主题
+
+# =========================
+# 删除无用主题
+# =========================
+
 sed -i '/CONFIG_PACKAGE_luci-theme-material=y/d' .config
+
 sed -i '/CONFIG_PACKAGE_luci-theme-openwrt-2020=y/d' .config
 
 
-# 删除旧缓存
+
+# =========================
+# 清理 LuCI缓存
+# =========================
+
 rm -rf tmp/luci-modulecache
 rm -f tmp/luci-indexcache
